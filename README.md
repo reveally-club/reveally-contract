1e235a99686567ae6400d97e68ed1449b2de8091
dilrong <dilrong@dilrong.com>
2022-11-22 22:02:45 +0900

    feat: matic setup

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# Sample Hardhat Project
-
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
+# Reveally.club Contract
+This repo contains all the contracts used in Reveally.club.
 
 ```shell
 npx hardhat help
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,17 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {},
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY!]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
 };
 
 export default config;
